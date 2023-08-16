9351b7fb9e190fa15e383cea8800590141621d1b
Dérick Rangel <84458463+DKRANGEL@users.noreply.github.com>
2023-08-16 22:16:41 +0000

    commit

--- Cursos/Bootstrap_5.1/first.js
+++ Cursos/Bootstrap_5.1/first.js
@@ -1,1 +1,12 @@
-let
+let coisas =  Array()
+
+coisas['pessoas'] = Array()
+coisas['frutas'] = Array()
+
+coisas['pessoas'][0] = 'Gabriel'
+coisas['pessoas'][1] = 'Dérick'
+
+coisas['frutas'][0] = 'Banana'
+coisas['frutas'][1] = 'Maçã'
+
+console.log(coisas);
