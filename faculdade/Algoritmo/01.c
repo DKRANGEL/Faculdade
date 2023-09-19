f98c9d0261a5d5f10cc7475b34e70902142eeb42
Dérick Rangel <84458463+DKRANGEL@users.noreply.github.com>
2023-09-19 00:30:33 +0000

    C_01

--- faculdade/Algoritmo/01.c
+++ faculdade/Algoritmo/01.c
@@ -3,5 +3,21 @@
 int main()
 {
 
-    printf("Hello World!!");
+    float n1, n2, n3, media;
+
+    printf("Digite o 1° Numero: ");
+    scanf("%f", &n1);
+
+    printf("Digite o 2° Numero: ");
+    scanf("%f", &n2);
+
+    printf("Digite o 3° Numero: ");
+    scanf("%f", &n3);
+
+    media = (n1 + n2 + n3) / 3;
+
+    printf("A média é %.2f", media);
+
+    return 0;
+   
 } 
